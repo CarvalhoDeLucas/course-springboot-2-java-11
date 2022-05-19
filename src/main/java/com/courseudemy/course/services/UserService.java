4ff03ea99f72037f6ff62c4b90bfe754055af5c5
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-05-19 15:52:18 -0300

    User update

--- src/main/java/com/courseudemy/course/services/UserService.java
+++ src/main/java/com/courseudemy/course/services/UserService.java
@@ -11,10 +11,10 @@ import com.courseudemy.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
@@ -23,12 +23,24 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		userRepository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = userRepository.getById(id);
+		updateDate(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateDate(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
